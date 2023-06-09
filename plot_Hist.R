a5b00fb618cc8e09a5621c9d6b9102d994c2d810
Quang C. Huynh <quang@bluematterscience.com>
2023-06-08 18:20:21 -0700

    Add stock codes to figures

--- plot_Hist.R
+++ plot_Hist.R
@@ -1,5 +1,6 @@
 
 library(MSEtool)
+
 source("analysis_fn.R")
 
 
@@ -14,39 +15,79 @@ SBall <- lapply(stocks, function(i) {
   YESH <- LRP$Assess$Year
   dep <- LRP$RPts$SB[LRP$RPts$Year == YESH]/LRP$RPts$SB[1]
   LRP$RPts %>% 
-    mutate(YESH = YESH, dep = dep) %>%
-    select(Year, SB, Stock, YESH, dep)
+    select(Year, SB, Stock, Removals) %>%
+    mutate(YESH = YESH, dep = dep, code = i)
 }) %>% bind_rows()
 
-lev_stock <- local({
-  x <- group_by(SBall, Stock) %>% summarise(YESH = unique(YESH), dep = unique(dep))
-  Stock <- x$Stock[order(x$dep)]
+stock_order <- SBall %>%
+  group_by(Stock) %>%
+  summarise(dep = mean(dep), code = unique(code)) %>%
+  arrange(dep) %>%
+  mutate(n = 1:n(),
+         Stock2 = paste0("\"(", n, ")\"~", Stock)) %>%
+  select(-dep)
+
+SBout <- left_join(SBall, stock_order, by = c("Stock", "code"))
+
+make_SB_figure <- function(SBout, stocks, lim = c("YESH", "all")) {
+  lim <- match.arg(lim)
   
-  x$Stock <- factor(x$Stock, levels = x$Stock[order(x$dep)])
-  x
-})
+  g <- lapply(stocks, function(sp) {
+    dframe <- SBout %>%
+      filter(Stock == sp) %>%
+      select(Year, SB, Removals, Stock2, YESH) %>%
+      rename(Catch = Removals) %>%
+      reshape2::melt(id.vars = c("Year", "Stock2", "YESH")) %>%
+      mutate(SB = ifelse(variable == "SB", value, NA_real_),
+             Catch = ifelse(variable == "SB", NA_real_, value))
+    
+    dframe_yesh <- filter(dframe, Year <= YESH)
+    dframe_future <- filter(dframe, Year > YESH)
+    g_out <- dframe_yesh %>%
+      ggplot(aes(x = Year)) + 
+      geom_line(aes(y = SB)) +
+      geom_col(colour = "black", linewidth = 0.1, fill = "grey40", width = 1, aes(y = Catch)) + 
+      geom_point(data = dframe %>% filter(Year == YESH, variable == "SB"), aes(y = SB)) +
+      facet_grid(vars(variable), 
+                 vars(Stock2), 
+                 scales = "free_y", 
+                 switch = "y",
+                 labeller = label_parsed) +
+      expand_limits(y = 0) +
+      #labs(x = "", y = "") +
+      theme_bw() + 
+      theme(strip.background = element_rect(fill = NA, colour = NA), 
+            axis.title = element_blank(),
+            panel.spacing = unit(0, "in"),
+            strip.text.y = element_blank(),
+            axis.text.y = element_blank(),
+            #axis.text.x = element_text(angle = 45),
+            axis.ticks.length.y = unit(0, "in"))
+    
+    if (lim == "all") {
+      g_out <- g_out +
+        geom_line(data = dframe_future, aes(y = SB), linetype = 4) +
+        geom_col(data = dframe_future, aes(y = Catch), width = 1, linewidth = 0.1, colour = "black", fill = "white")
+    }
+    g_out
+  })
+  cowplot::plot_grid(plotlist = g, ncol = 4, align = "hv")
+}
 
-SBall$Stock <- SBall$Stock %>% factor(levels = levels(lev_stock$Stock))
 
+g <- make_SB_figure(SBout, stocks = stock_order$Stock[1:2]) %>%
+  ggpubr::annotate_figure(bottom = "Year")
+ggsave("Figures/meta/SB_with_catch.png", g, height = 8, width = 7)
 
-g <- ggplot(SBall, aes(Year, SB)) + 
-  geom_line() +
-  theme_bw() + 
-  geom_vline(data = lev_stock, aes(xintercept = YESH), linetype = 2) + 
-  facet_wrap(vars(Stock),
-             ncol = 4,
-             scales = "free",
-             labeller = label_parsed) +
-  expand_limits(y = 0) +
-  theme(strip.background = element_rect(fill = NA, colour = NA),
-        panel.spacing = unit(0, "in"))
-ggsave("Figures/meta/SB.png", g, height = 4)
+g <- make_SB_figure(SBout, stocks = stock_order$Stock[1:2], lim = "all") %>%
+  ggpubr::annotate_figure(bottom = "Year")
+ggsave("Figures/meta/SB_with_catch_all.png", g, height = 8, width = 7)
 
-
-
+# All case studies
 for(i in stocks) {
   
   LRP <- readRDS(file = paste0("LRP/LRP_", i, ".rds"))
+  LRP[["RPvars"]][["Stock"]] <- LRP[["RPts"]]$Stock <- stock_order %>% filter(code == i) %>% pull(Stock2)
   
   message(i)
   message("Year range: ", range(LRP$RPts$Year) %>% paste0(collapse = " - "))
@@ -76,6 +117,7 @@ case_studies <- stocks
 for(i in case_studies) {
   
   LRP <- readRDS(file = paste0("LRP/LRP_", i, ".rds"))
+  LRP[["RPvars"]][["Stock"]] <- LRP[["RPts"]]$Stock <- stock_order %>% filter(code == i) %>% pull(Stock2)
   
   message(i)
   
@@ -84,11 +126,10 @@ for(i in case_studies) {
   g2 <- ggSR_LRP(LRP, title = FALSE, panel_labs = c("SB[0.5~Rmax]", "SB[0.9~R/S]"),
                  letter.legend = paste0("(", letters[4:5], ")"))
   
-  g3 <- ggSP(LRP, SPB = FALSE, title = FALSE, letter.legend = "(f)")
+  g3 <- ggSP(LRP, SPB = TRUE, title = FALSE, letter.legend = "(f)")
   
   gright <- cowplot::plot_grid(g2, g3, ncol = 1)
   gout <- cowplot::plot_grid(g1, gright)
   
   ggsave(paste0("Figures/case_studies/", i, ".png"), gout, height = 5, width = 8)
-  
 }
